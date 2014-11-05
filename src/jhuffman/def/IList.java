27a6880b6cd9078184f74f02ac561e622b1fd6ae
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-05 13:23:47 -0300

    Comentarios by Gonzalo

--- src/jhuffman/def/IList.java
+++ src/jhuffman/def/IList.java
@@ -4,20 +4,30 @@ import java.util.Comparator;
 
 public class IList 
 {
+	Node p;
+	
+	//desenlazar y devolver puntero, no liberarlo
 	public Node removeFirstNode()
 	{
 		return null;
 	}
-	
+	//inserta ordenado
 	public void addNode(Node n)
 	{
+		
 	}
+	
+	//sacar 2 nodos a la lista y se crea un "pequeño arbol bnario" con las 2 hojas
 	
 	public ITree toTree()
 	{
 		return null;
 	}
 	
+	/*la interfaz es una definicion de una clase, la funcion comparator es una
+	 * interfaz
+	 * 
+	 */
 	public Comparator<Node> getComparator()
 	{
 		return null;
--- src/jhuffman/def/ITable.java
+++ src/jhuffman/def/ITable.java
@@ -1,8 +1,11 @@
 package jhuffman.def;
 
-
+// representa la tabla
+// hacer una clase qe representa los campos de la tabla (analogo a un estrutura)
+// hacer un array de clases
 public class ITable
 {
+	
 	public void addCount(int c)
 	{	
 	}
@@ -12,14 +15,19 @@ public class ITable
 		return 0;
 	}
 	
+	// a partir de los datos del array se crea la lista enlazada con la funcion add
 	public IList createSortedList()
 	{
+		
 		return null;
 	}
 	
+	// se lee el arbol hoja por hoja y se cargan los codigos en la tabla
 	public void loadHuffmanCodes(ITree root)
 	{
+		
 	}
+	
 	
 	public ICode getCode(int c)
 	{
