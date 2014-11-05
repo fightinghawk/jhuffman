e2d0ef632352b55270e571cc6f8bfd4050332ab0
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-05 10:27:40 -0300

    Utree Desarrollado

--- src/jhuffman/def/ITree.java
+++ src/jhuffman/def/ITree.java
@@ -1,18 +1,28 @@
 package jhuffman.def;
 
+import jhuffman.util.UTree;
+
 public class ITree
 {
+	Node raiz;
+	UTree p;
+	
 	public void setRoot(Node root)
 	{	
+		raiz = root;
+		p = new UTree(raiz);
 	}
 	
 	public Node getRoot()
 	{
-		return null;
+		return raiz;
 	}
 	
 	public Node next(ICode cod)
 	{
-		return null;
+		StringBuffer buff = new StringBuffer();
+		Node aux = p.next(buff);
+		cod.fromString(buff.toString());
+		return aux;
 	}
 }
