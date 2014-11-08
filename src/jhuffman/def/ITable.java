6dbe038911a99268a16b67548b9266b25f141347
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-08 15:10:48 -0300

    Itable.java (Falta createSortedList)

--- src/jhuffman/def/ITable.java
+++ src/jhuffman/def/ITable.java
@@ -5,32 +5,44 @@ package jhuffman.def;
 // hacer un array de clases
 public class ITable
 {
+	public class Table
+	{
+		long n=0;
+		ICode cod;
+	}
 	
+	Table arr[] = new Table[256];
 	public void addCount(int c)
 	{	
+		arr[c].n++;
 	}
 	
 	public long getCount(int c)
 	{
-		return 0;
+		return arr[c].n;
 	}
 	
 	// a partir de los datos del array se crea la lista enlazada con la funcion add
 	public IList createSortedList()
 	{
-		
 		return null;
 	}
 	
 	// se lee el arbol hoja por hoja y se cargan los codigos en la tabla
-	public void loadHuffmanCodes(ITree root)
+	public void loadHuffmanCodes(ITree tree)
 	{
-		
+		ICode codHuffman = null;
+		Node hoja = tree.next(codHuffman);
+		while (hoja != null)
+		{
+			int c = hoja.getC();
+			arr[c].cod = codHuffman;
+		}
 	}
 	
 	
 	public ICode getCode(int c)
 	{
-		return null;
+		return arr[c].cod;
 	}
 }
