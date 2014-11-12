31c2c4b374703ca0547a57e759d8bf14d3aa6986
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-12 14:36:08 -0300

    Ilist Node

--- src/jhuffman/def/IList.java
+++ src/jhuffman/def/IList.java
@@ -4,17 +4,42 @@ import java.util.Comparator;
 
 public class IList 
 {
+	///Hay que usar las funciones que tiene asociada node para asignar
+	// el siguiente para los nodos de la lista
+	
+	
+	private static final Node NULL = null;
 	Node p;
 	
 	//desenlazar y devolver puntero, no liberarlo
 	public Node removeFirstNode()
 	{
-		return null;
+		Node aux=p;
+		p=p.getSig();
+		return aux;
 	}
 	//inserta ordenado
 	public void addNode(Node n)
 	{
-		
+		Node nuevo= new Node();
+		nuevo.setC(n.getC());
+		nuevo.setSig(NULL);
+		Node ant=NULL;
+		Node aux=p;
+		while( aux!=NULL && aux.getC()<=n.getC())
+		{
+		ant = aux;
+		aux = aux.getSig();
+		}
+		if( ant==NULL )
+		{
+		p = nuevo;
+		}
+		else
+		{
+		ant.setSig(nuevo);
+		}
+		nuevo.setSig(aux);
 	}
 	
 	//sacar 2 nodos a la lista y se crea un "pequeño arbol bnario" con las 2 hojas
