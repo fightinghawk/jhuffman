5ce9d8e699658c641d338805206886ae07e0ef3f
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-12 14:36:08 -0300

    IFILEInput
    Signed-off-by: Villalba Caceres Vicente Javier
    <jvillalba@piensamates.com>

--- src/jhuffman/def/IFileInput.java
+++ src/jhuffman/def/IFileInput.java
@@ -1,24 +1,64 @@
 
 package jhuffman.def;
 
+import java.io.File;
+import java.io.FileInputStream;
+import jhuffman.util.UFile;
+
 public class IFileInput
 {
-	public ITable createTable()
-	{
-		return null;
-	}
+	File file = null;
+	FileInputStream fis = null;
 	
 	public void setFilename(String filename)
-	{
+	{	
+		file = new File(filename);
 	}
 	
 	public String getFilename()
-	{
-		return null;
+	{		
+		return file.getPath();
 	}
 	
 	public long getLength()
 	{
-		return 0;
+		return file.length();
+	}
+	
+	public ITable createTable()
+	{
+		ITable iT = new ITable();
+			
+		try
+		{
+			// abro el archivo para leer los bits
+			fis = new FileInputStream(file);			
+			UFile uFile = new UFile(fis);
+						
+			int bit = uFile.readBit();
+			while( bit>=0 )
+			{
+				iT.addCount(bit);
+				bit = uFile.readBit();
+			}			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			try
+			{
+				if(fis!=null) fis.close();
+			}
+			catch(Exception e2)
+			{
+				e2.printStackTrace();
+				throw new RuntimeException(e2);
+			}
+		}
+		return iT;
 	}
 }
