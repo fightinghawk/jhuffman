6bfdfd38f1d5f987a78335b17385bdab99270011
Villalba Caceres Vicente Javier <jvillalba@piensamates.com>
2014-11-16 12:26:47 -0300

    Grabar Caracter y Longitud del Código de Huffman

--- src/jhuffman/def/IFileCode.java
+++ src/jhuffman/def/IFileCode.java
@@ -1,13 +1,55 @@
 package jhuffman.def;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import jhuffman.util.UFile;
+
 public class IFileCode
 {
+	File file = null;
+	FileOutputStream fos = null;
+	
 	public void setFilename(String f)
-	{
+	{		
+		file = new File(f);
 	}
 	
 	public void save(ITable table)
 	{
+		try
+		{
+			fos = new FileOutputStream(file);
+			UFile uFile = new UFile(fos);
+			
+			for(int i=0; i<256; i++)
+			{
+				uFile.writeBit(i);
+				int len = table.arr[i].cod.getLength();
+				uFile.writeBit(len);
+				for( int j=0; i<table.arr[i].cod.len; j++)
+				{
+					int bit = table.arr[i].cod.getBitAt(j);			
+					uFile.writeBit(bit);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		finally
+		{
+			try
+			{
+				if(fos!=null) fos.close();
+			}
+			catch(Exception e2)
+			{
+				e2.printStackTrace();
+				throw new RuntimeException(e2);
+			}
+		}
 	}
 
 	public ITree load()
